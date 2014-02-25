fdfd2d4be58ea668f925cebc416e80c3a44f2786
AliceWaddicor <alice.waddicor@endjin.com>
2014-02-25 10:59:14 +0000

    Tests for metaWeblog getCategories complete

---
+++ Solutions/Endjin.Web.MetaWeblog/Mappers/XmlRpc/Request/XmlRpcRequestToGetCategoriesRequestMapper.cs
@@ -0,0 +1,30 @@
+﻿namespace Endjin.Web.MetaWeblog.Mappers.XmlRpc.Request
+{
+    #region Using Directives
+
+    using System.Linq;
+
+    using Endjin.Web.MetaWeblog.Contracts.Mappers;
+    using Endjin.Web.MetaWeblog.Domain.MetaWeblog.Request;
+    using Endjin.Web.MetaWeblog.Domain.XmlRpc;
+
+    #endregion
+
+    public class XmlRpcRequestToGetCategoriesRequestMapper : IMapper<Request, GetCategoriesRequest>
+    {
+        public GetCategoriesRequest MapFrom(Request input)
+        {
+            return new GetCategoriesRequest
+            {
+                AppKey = input.Params.ElementAt(0).RequestValue.String,
+                Password = input.Params.ElementAt(2).RequestValue.String,
+                UserName = input.Params.ElementAt(1).RequestValue.String,
+            };
+        }
+
+        public object MapFrom(object input)
+        {
+            return this.MapFrom((Request)input);
+        }
+    }
+}
---
+++ Solutions/Endjin.Web.MetaWeblog/Mappers/XmlRpc/Response/CategoryInfoToXmlRpcResponseMapper.cs
@@ -0,0 +1,99 @@
+﻿using System.Globalization;
+
+namespace Endjin.Web.MetaWeblog.Mappers.XmlRpc.Response
+{
+    #region Using Directives
+
+    using System.Collections.Generic;
+
+    using Endjin.Web.MetaWeblog.Contracts.Mappers;
+    using Endjin.Web.MetaWeblog.Domain.MetaWeblog.Response;
+    using Endjin.Web.MetaWeblog.Domain.XmlRpc;
+
+    #endregion
+
+    public class CategoryInfoToXmlRpcResponseMapper : IMapper<List<CategoryInfo>, Response>
+    {
+        public Response MapFrom(List<CategoryInfo> input)
+        {
+            var response = new Response();
+
+            foreach (var categoryInfo in input)
+            {
+                response.Params.Add(new ResponseParams
+                {
+                    Param = new ResponseParam
+                    {
+                        Value = new ResponseData
+                        {
+                            Data = new List<ResponseDataItem>
+                            {
+                                new ResponseDataItem
+                                {
+                                    Value = new ResponseParamValue
+                                    {
+                                        Member = new List<Member>
+                                        {
+                                            new Member
+                                            {
+                                                Name = "description",
+                                                Value = new MemberValue
+                                                {
+                                                    ValueChoice = MemberValue.ValueType.@string,
+                                                    Value = categoryInfo.description
+                                                }
+                                            },
+                                            new Member
+                                            {
+                                                Name = "categoryid",
+                                                Value = new MemberValue
+                                                {
+                                                    ValueChoice = MemberValue.ValueType.@string,
+                                                    Value = categoryInfo.categoryid.ToString(CultureInfo.InvariantCulture)
+                                                }
+                                            },
+                                            new Member
+                                            {
+                                                Name = "title",
+                                                Value = new MemberValue
+                                                {
+                                                    ValueChoice = MemberValue.ValueType.@string,
+                                                    Value = categoryInfo.title
+                                                }
+                                            },
+                                            new Member
+                                            {
+                                                Name = "htmlUrl",
+                                                Value = new MemberValue
+                                                {
+                                                    ValueChoice = MemberValue.ValueType.@string,
+                                                    Value = categoryInfo.htmlUrl
+                                                }
+                                            },
+                                            new Member
+                                            {
+                                                Name = "rssUrl",
+                                                Value = new MemberValue
+                                                {
+                                                    ValueChoice = MemberValue.ValueType.@string,
+                                                    Value = categoryInfo.rssUrl
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+
+            return response;
+        }
+
+        public object MapFrom(object input)
+        {
+            return this.MapFrom((List<CategoryInfo>)input);
+        }
+    }
+}
